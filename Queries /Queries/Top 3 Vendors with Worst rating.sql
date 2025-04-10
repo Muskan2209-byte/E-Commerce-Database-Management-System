8de7a35182d44767c4b3775c9f3731e39ccd732f
Muskan2209-byte <muskans2209@gmail.com>
2025-04-10 12:36:26 +0530

    Create Top 3 Vendors with Worst rating.sql

---
+++ Queries /Queries/Top 3 Vendors with Worst rating.sql
@@ -0,0 +1,18 @@
+﻿use OnlineShopping
+
+
+select TOP 3 Name,Contact,Email,Total_bad_ratings
+from Vendor
+inner join(
+select VendorID,COUNT(rating) as Total_bad_ratings
+from VendorProduct
+inner join(
+select Rating,Comment,OrderedProduct.VendorProductID
+from OrderedProduct
+inner join(
+select Rating,Comment,OrderedProductID
+from Review
+where Rating<3) r on r.OrderedProductID=OrderedProduct.OrderedProductID) 
+vr on vr.VendorProductID=VendorProduct.VendorProductID group by VendorID)
+vrt on vrt.VendorID=Vendor.VendorID
+Order by Total_bad_ratings desc
